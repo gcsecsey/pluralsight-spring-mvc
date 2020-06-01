625f83ec1c2738cbcde3dc8abad235d8c20f4a10
gcsecsey <gcsecsey@gmail.com>
2020-06-01 18:53:58 +0200

    8.5 add template and controller

--- src/main/java/com/pluralsight/springmvcconference/controller/GreetingController.java
+++ src/main/java/com/pluralsight/springmvcconference/controller/GreetingController.java
@@ -13,4 +13,10 @@ public class GreetingController {
     model.put("message", "Hello World"); // JSP var name
     return "greeting"; // pass it back to JSP
   }
+
+  @GetMapping("thyme")
+  public String thyme(Map<String, Object> model) {
+    model.put("message", "Hello Thymeleaf"); // JSP var name
+    return "thyme"; // pass it back to JSP
+  }
 }
