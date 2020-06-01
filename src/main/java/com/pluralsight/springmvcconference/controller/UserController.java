f74b70653756b2e4618b883f9238d1f7d7535b48
gcsecsey <gcsecsey@gmail.com>
2020-06-01 19:54:47 +0200

    10.4 PostMapping

--- src/main/java/com/pluralsight/springmvcconference/controller/UserController.java
+++ src/main/java/com/pluralsight/springmvcconference/controller/UserController.java
@@ -2,6 +2,7 @@ package com.pluralsight.springmvcconference.controller;
 
 import com.pluralsight.springmvcconference.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,12 @@ public class UserController {
     user.setFirstName(firstName);
     user.setLastName(lastName);
     user.setAge(age);
+    return user;
+  }
+
+  @PostMapping("/user")
+  public User postUser(User user) {
+    System.out.println("User firstname: " + user.getFirstName());
     return user;
   }
 }
