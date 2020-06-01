879c2966f6c22a1074b27758888415ba46d76b2e
gcsecsey <gcsecsey@gmail.com>
2020-06-01 13:21:27 +0200

    Initial commit

---
+++ src/main/java/com/pluralsight/springmvcconference/SpringmvcConferenceApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.springmvcconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcConferenceApplication.class, args);
+	}
+
+}
