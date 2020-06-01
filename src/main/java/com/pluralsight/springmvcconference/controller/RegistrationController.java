e5e4ee8c0837a930bcbb9c250ce0ecb94d38ad32
gcsecsey <gcsecsey@gmail.com>
2020-06-01 16:16:25 +0200

    5.6 add modelattribute

--- src/main/java/com/pluralsight/springmvcconference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/springmvcconference/controller/RegistrationController.java
@@ -1,7 +1,9 @@
 package com.pluralsight.springmvcconference.controller;
 
+import com.pluralsight.springmvcconference.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Map;
 
@@ -9,7 +11,7 @@ import java.util.Map;
 public class RegistrationController {
 
   @GetMapping("registration")
-  public String getRegistration(Map<String, Object> model) {
+  public String getRegistration(@ModelAttribute("registration")Registration registration) {
       return "registration";
     }
 }
