21a2cef5aa58fc166b69a78da3141b8cb6900058
gcsecsey <gcsecsey@gmail.com>
2020-06-01 16:29:38 +0200

    5.6 update controller

--- src/main/java/com/pluralsight/springmvcconference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/springmvcconference/controller/RegistrationController.java
@@ -4,8 +4,7 @@ import com.pluralsight.springmvcconference.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
@@ -13,5 +12,11 @@ public class RegistrationController {
   @GetMapping("registration")
   public String getRegistration(@ModelAttribute("registration")Registration registration) {
       return "registration";
-    }
+  }
+
+  @PostMapping("registration")
+  public String addRegistration(@ModelAttribute("registration")Registration registration) {
+    System.out.println("Registration: " + registration.getName());
+    return "registration";
+  }
 }
